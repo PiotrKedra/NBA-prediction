323149ac5ca2e87e02489f2b772d25329705a21e
piotrkedra <piotr.k9750@gmail.com>
2020-11-16 22:39:51 +0100

    load dataset v2

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
+from matplotlib import pyplot
 
 test_data = pd.read_csv('resource/games.csv')
-
 print(test_data)
+
+test_data.plot(kind='scatter', x='GAME_ID', y='REB_away')
+pyplot.show()
 
